9942a1d573f868c49ab6b33e4b1197f905f20ee5
shounakdatta <shounak14datta@gmail.com>
2019-06-02 18:18:29 -0400

    Updated port

--- scrape.js
+++ scrape.js
@@ -30,5 +30,7 @@ app.get('/api/stocks', async (req, res) => {
     }
 })
 
-app.listen(3000, () => console.log('Listening on PORT 3000...'))
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
